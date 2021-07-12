b626b643663476745f7798284d2ef98e65a3ae2f
Mike Crowe <drmikecrowe@gmail.com>
2021-07-12 07:14:42 -0400

    Fixing es6 detection

--- packages/eslint-config/lib/index.js
+++ packages/eslint-config/lib/index.js
@@ -1,7 +1,12 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-  ],
+  extends: ['eslint:recommended'],
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 8,
+  },
 
   rules: {
     /**
