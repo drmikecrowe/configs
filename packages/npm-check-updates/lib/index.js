5152b8f022d27061daf17be6a4d708502dc6c469
Mike Crowe <drmikecrowe@gmail.com>
2021-07-29 06:02:36 -0400

    feat: add ncu package manager detection

--- packages/npm-check-updates/lib/index.js
+++ packages/npm-check-updates/lib/index.js
@@ -4,9 +4,14 @@
  * @see https://github.com/raineorshine/npm-check-updates
  */
 
+const fs = require('fs');
+
+const yarn = fs.existsSync('yarn.lock');
+const pnpm = fs.existsSync('pnpm-lock.yaml')
+
 module.exports = {
   upgrade: true,
-  packageManager: "yarn",
+  packageManager: yarn ? "yarn" : pnpm ? 'pnpm' : 'npm',
   target: "minor",
   reject: ["typescript"],
 };
